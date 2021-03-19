27efd3a33764d41dbf3f5d746e69871cd18e83c7
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-03-19 12:55:41 -0300

    Desafio 02 resolvido

---
+++ Desafio 02/soma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio_02
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um número");
+            int B = int.Parse(Console.ReadLine());
+
+            int X =  A + B;
+
+            Console.WriteLine("X = " + X);
+
+        }
+    }
+}
